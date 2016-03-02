48a0e4ec82b3ff6b59d28f27fa633016487b9fcc
SallyMo <liangz3398848@163.com>
2016-03-02 15:52:19 +0800

    第二个开机引导demo

---
+++ app/src/main/java/com/example/jojo/guiddemo2/ViewPaperAdapter.java
@@ -0,0 +1,51 @@
+package com.example.jojo.guiddemo2;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+
+ * create an instance of this fragment.
+ */
+public class ViewPaperAdapter extends PagerAdapter {
+    private ArrayList<View> views;
+
+    public ViewPaperAdapter(ArrayList<View> views) {
+        this.views = views;
+    }
+
+    @Override
+    public int getCount() {
+        if (views != null) {
+            return views.size();
+        } else
+            return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(View arg0, Object arg1) {
+        return (arg0 == arg1);
+    }
+
+    @Override
+    public void destroyItem(View container, int position, Object object) {
+        ((ViewPager) container).removeView(views.get(position));
+    }
+
+    @Override
+    public Object instantiateItem(View container, int position) {
+        ((ViewPager) container).addView(views.get(position), 0);
+        return views.get(position);
+    }
+
+}
